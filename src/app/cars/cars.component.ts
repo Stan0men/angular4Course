9d9ebc7d7454c36e71f6dba07331e8fd3119e231
Stan0men <stan0men@yahoo.com>
2019-04-30 14:18:51 +0300

    13 done (pipes)

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,9 +9,20 @@ import {templateRefExtractor} from '@angular/core/src/render3';
 export class CarsComponent {
   carName = '';
   addCarStatus = false;
+  cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bentley'];
+  items = [
+    new Date(2015, 3, 4),
+    new Date(2011, 2, 7),
+    new Date(2017, 4, 6),
+    new Date(2000, 6, 8)
+
+
+  ];
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   }
 
 }
