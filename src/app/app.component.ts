444993f58b63dc5e98cbe7b1679e6eb113c25483
Stan0men <stan0men@yahoo.com>
2019-05-12 11:42:30 +0300

    25 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
 import {Component} from '@angular/core';
-import {assertNumber} from '@angular/core/src/render3/assert';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <div class="col-xs-8 col-xs-offset-2">
+        <h2>{{name}}</h2>
+        <h2>{{name | lowercase}}</h2>
+        <h2>{{name | uppercase}}</h2>
+        <h2>{{name | slice:0:3 }}</h2>
+        <hr>
+        <h2>{{pi | number}}</h2>
+      </div>
+    </div>
+
+  `
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы.';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-
-  onClick(item: number) {
-    this.current = item;
-  }
-
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+
 import {AppComponent} from './app.component';
-import {BackgroundDirective} from './directives/background.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundDirective,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
